0764ac5a78d7dfa2771b41cd451148f7a3707a1a
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:44:26 +0700

    Create latihan2modul5.java

---
+++ latihan2modul5.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane; 
+public class latihan2modul5 {
+    public static void main( String[] args ){ 
+        String word1,word2,rata = " "; 
+
+        word1 = JOptionPane.showInputDialog("Enter Word1");
+        word2 = JOptionPane.showInputDialog("Enter Word2"); 
+
+        String msg = rata + " "; 
+
+        JOptionPane.showMessageDialog(null, msg); 
+
+ } 
+}
